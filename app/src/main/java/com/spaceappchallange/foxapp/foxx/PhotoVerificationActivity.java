8941ee0bf8d3316ff74deee59082a49a1e82c85a
Michał Wądołowski <michwadol@gmail.com>
2017-04-29 22:15:26 +0200

    adding view with only layout

---
+++ app/src/main/java/com/spaceappchallange/foxapp/foxx/PhotoVerificationActivity.java
@@ -0,0 +1,13 @@
+package com.spaceappchallange.foxapp.foxx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_verification);
+    }
+}
