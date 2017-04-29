97a1509eb209d74cb1f05d6ba77c0396f0a518df
Dawid Paluchowski <darkon.darkon@gmail.com>
2017-04-29 22:47:43 +0200

    Refactoring activity names, added rest of the activities

---
+++ app/src/main/java/com/spaceappchallange/foxapp/foxx/CameraActivity.java
@@ -0,0 +1,136 @@
+package com.spaceappchallange.foxapp.foxx;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CameraActivity extends Activity implements SurfaceHolder.Callback {
+
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    PictureCallback jpegCallback;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_camera);
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        surfaceHolder.addCallback(this);
+
+        // deprecated setting, but required on Android versions prior to 3.0
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        jpegCallback = new PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                FileOutputStream outStream;
+                try {
+                    outStream = new FileOutputStream(String.format("/sdcard/%d.jpg", System.currentTimeMillis()));
+                    outStream.write(data);
+                    outStream.close();
+                    Log.d("Log", "onPictureTaken - wrote bytes: " + data.length);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                }
+                Toast.makeText(getApplicationContext(), "Picture Saved", Toast.LENGTH_LONG).show();
+                refreshCamera();
+            }
+        };
+    }
+
+    public void captureImage(View v) throws IOException {
+        //take the picture
+        camera.takePicture(null, null, jpegCallback);
+    }
+
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+        // start preview with new settings
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        refreshCamera();
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            // open the camera
+            camera = Camera.open();
+        } catch (RuntimeException e) {
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+        Camera.Parameters param = camera.getParameters();
+
+        // modify parameter
+        Camera.Size supportedPictureSize = camera.getParameters().getSupportedPictureSizes().get(0);
+        param.setPreviewSize(supportedPictureSize.width, supportedPictureSize.height);
+
+        camera.setParameters(param);
+        try {
+            // The Surface has been created, now tell the camera where to draw
+            // the preview.
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+        //adjustRotation();
+        int correction = 90;
+        camera.setDisplayOrientation(correction);
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // stop preview and release camera
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+}
--- app/src/main/java/com/spaceappchallange/foxapp/foxx/InfoActivity.java
+++ app/src/main/java/com/spaceappchallange/foxapp/foxx/InfoActivity.java
@@ -3,11 +3,11 @@ package com.spaceappchallange.foxapp.foxx;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_info);
     }
 }
