76ce8a9d54b145f6d712c721401c5b8d5ed9e7ae
Michał Wądołowski <michwadol@gmail.com>
2017-04-30 07:15:39 +0200

    dont remember

---
+++ app/src/main/java/com/spaceappchallange/foxapp/foxx/DevOnlyActivity.java
@@ -0,0 +1,62 @@
+package com.spaceappchallange.foxapp.foxx;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.Inet4Address;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class DevOnlyActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private Button button;
+    String species;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_only);
+        Intent oldIntention = getIntent();
+        species = oldIntention.getStringExtra("URI");
+        mTextMessage = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = mTextMessage.getText().toString();
+                URL url = null;
+                try {
+                    url = new URL(text);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                HttpURLConnection conn = null;
+                try {
+                    conn = (HttpURLConnection)url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                conn.setReadTimeout(20000);
+                conn.setConnectTimeout(15000);
+                try {
+                    conn.setRequestMethod("GET");
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/spaceappchallange/foxapp/foxx/PhotoVerificationActivity.java
+++ app/src/main/java/com/spaceappchallange/foxapp/foxx/PhotoVerificationActivity.java
@@ -6,6 +6,9 @@ import android.graphics.BitmapFactory;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -13,9 +16,14 @@ import java.io.File;
 
 public class PhotoVerificationActivity extends AppCompatActivity {
     String name;
+    Button verify;
+    String species;
+    PhotoVerificationActivity self;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        self = this;
         setContentView(R.layout.activity_photo_verification);
         ImageView imageView = (ImageView) this.findViewById(R.id.imageView);
         Intent oldIntent = getIntent();
@@ -23,6 +31,16 @@ public class PhotoVerificationActivity extends AppCompatActivity {
         File imgFile = new File(name);
         Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
         imageView.setImageBitmap(myBitmap);
-        //imageView.setRotation(90);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        species = editText.getText().toString();
+        verify = (Button) findViewById(R.id.button3);
+        verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newIntention = new Intent(self, DevOnlyActivity.class);
+                newIntention.putExtra("URI", species);
+                startActivity(newIntention);
+            }
+        });
     }
 }
